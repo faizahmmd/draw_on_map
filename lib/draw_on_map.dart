2c8d843395726ade2915abd329b4917d5f2f5de6
Faiz Ahammed <faizahmmd@Faizs-MacBook-Pro.local>
2023-02-12 02:20:58 +0530

    removed mapController access from outside

--- lib/draw_on_map.dart
+++ lib/draw_on_map.dart
@@ -4,16 +4,16 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapWidget extends StatefulWidget {
-  final MapController mapController;
   final double? minZoom, maxZoom;
   final double zoom, strokeWidth, borderStrokeWidth;
   final Color polylineColor, borderColor;
   final LatLng? center;
+  final LatLng currentUserLocation;
   final List<Marker>? markers;
-  final Positioned? polylineResetWidget;
+  final Positioned? polylineResetWidget, currentLocationFocusWidget;
   final ValueChanged drawnRouteLatLngList;
   final bool isDotted;
-  const MapWidget({Key? key, required this.mapController, this.minZoom, this.maxZoom, required this.zoom, this.center, this.markers, required this.polylineResetWidget, required this.drawnRouteLatLngList, required this.strokeWidth, required this.polylineColor, required this.borderStrokeWidth, required this.borderColor, required this.isDotted}) : super(key: key);
+  const MapWidget({Key? key, this.minZoom, this.maxZoom, required this.zoom, this.center, this.markers, required this.polylineResetWidget, required this.drawnRouteLatLngList, required this.strokeWidth, required this.polylineColor, required this.borderStrokeWidth, required this.borderColor, required this.isDotted, this.currentLocationFocusWidget, required this.currentUserLocation}) : super(key: key);
 
   @override
   State<MapWidget> createState() => _MapWidgetState();
@@ -22,12 +22,14 @@ class MapWidget extends StatefulWidget {
 class _MapWidgetState extends State<MapWidget> {
   List<LatLng> polylineLatLngList = [];
   bool startDrawing = false;
+  MapController mapController = MapController();
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         FlutterMap(
-            mapController: widget.mapController,
+            mapController: mapController,
             options: MapOptions(
                 zoom: widget.zoom,
                 minZoom: widget.minZoom,
@@ -74,14 +76,13 @@ class _MapWidgetState extends State<MapWidget> {
                 polylineLatLngList.clear();
               });
             },
-            child: widget.polylineResetWidget)
+            child: widget.polylineResetWidget),
+        GestureDetector(
+            onTap: (){
+             mapController.move(widget.currentUserLocation, widget.zoom);
+            },
+            child: widget.currentLocationFocusWidget)
       ],
     );
   }
-}
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
 }
